7baa56d04b3aabde6e02070fae392e6d882e0c31
vinhvp <vinhvp@dgroup.co>
2024-04-03 16:30:11 +0700

    feat: add 3 project for app Refs: restaurant, cover-diary, introduce

--- project-web/src/app/my-cover-diary/page.tsx
+++ project-web/src/app/my-cover-diary/page.tsx
@@ -1,6 +1,142 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { Button } from "react-bootstrap";
+import Table from "react-bootstrap/Table";
+
 const MyCoverDiary = () => {
-    return <div>My Cover Diary</div>;
+  const router = useRouter();
+
+  const handleButton = () => {
+    router.push("./");
   };
-  
-  export default MyCoverDiary;
-  
+
+  return (
+    <>
+      <title>MyCoverDiary</title>
+      <div>
+        <Button variant="danger" onClick={() => handleButton()}>
+          Back to Home
+        </Button>
+        <div className="mt-4">
+          <div
+            style={{
+              border: "2px solid black",
+              width: "40vw",
+              height: "80vh",
+            }}
+          >
+            <div
+              style={{
+                padding: "16px",
+              }}
+            >
+              <h1
+                style={{
+                  fontSize: "30px",
+                  color: "#4A88B2",
+                }}
+              >
+                COVER DESIGN
+              </h1>
+              <br />
+              <h2
+                style={{
+                  fontSize: "20px",
+                  color: "gray",
+                }}
+              >
+                FLYER TEMPLATE
+              </h2>
+              <p
+                style={{
+                  // width: "250px",
+                  textAlign: "justify",
+                  color: "gray",
+                }}
+              >
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing
+              </p>
+              <br />
+              <h3
+                style={{
+                  color: "#4A88B2",
+                }}
+              >
+                Lorem Ipsum
+              </h3>
+              <hr></hr>
+              <br />
+            </div>
+            <div
+              style={{
+                backgroundImage:
+                  "url('https://i.pinimg.com/564x/49/8c/bb/498cbb37dace53c78aa238a9b512f354.jpg')",
+                height: "220px",
+              }}
+            ></div>
+            <img
+              style={{
+                position: "absolute",
+                top: "100px",
+                left: "400px",
+              }}
+              src="https://telegra.ph/file/4c01944eeefd8cd43b3aa.png"
+              alt="xuantuyen-education"
+            />
+          </div>
+        </div>
+      </div>
+      <Table striped bordered hover size="sm" className="mt-4">
+        <thead>
+          <tr>
+            <th>HTML</th>
+            <th>CSS</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <pre>
+                {`<div style="border: 2px solid black; width: 40vw; height: 80vh;">
+  <div style="padding: 16px;">
+    <h1 style="font-size: 30px; color: #4A88B2;">COVER DESIGN</h1>
+    <br />
+    <h2 style="font-size: 20px; color: gray;">FLYER TEMPLATE</h2>
+    <p style="text-align: justify; color: gray;">
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the
+      1500s, when an unknown printer took a galley of type and scrambled it to
+      make a type specimen book. It has survived not only five centuries, but
+      also the leap into electronic typesetting, remaining essentially
+      unchanged. It was popularised in the 1960s with the release of Letraset
+      sheets containing Lorem Ipsum passages, and more recently with desktop
+      publishing
+    </p>
+    <br />
+    <h3 style="color: #4A88B2;">Lorem Ipsum</h3>
+    <hr />
+    <br />
+  </div>
+  <div style="background-image: url('https://i.pinimg.com/564x/49/8c/bb/498cbb37dace53c78aa238a9b512f354.jpg'); height: 220px;"></div>
+  <img src="https://telegra.ph/file/4c01944eeefd8cd43b3aa.png" alt="xuantuyen-education" style="position: absolute; top: 100px; left: 400px;" />
+</div>
+`}
+              </pre>
+            </td>
+            <td>No CSS</td>
+          </tr>
+        </tbody>
+      </Table>
+    </>
+  );
+};
+
+export default MyCoverDiary;
